36984215dd2b059a88a5aa847c884fd661341716
pdelbert <pmarcadel@gmail.com>
2025-03-05 18:46:46 -0300

    Adding - Token Verification from Redux Store in utils/PrivateRoutes

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,8 +1,10 @@
+import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom'
+import { RootState } from '../state/store';
 
 const PrivateRoutes = () => {
-  const token = true;
-  return token ? <Outlet /> : <Navigate to="/" />
+  const login = useSelector((state: RootState) => state.login);
+  return login.token ? <Outlet /> : <Navigate to="/" />
 }
 
 export default PrivateRoutes
