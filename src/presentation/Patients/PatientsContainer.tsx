b31c282c9a16fe30452b1d1d140ae2fe49e5ae1c
pdelbert <pmarcadel@gmail.com>
2025-03-06 14:37:05 -0300

    Adding - Patients View and Container

--- src/presentation/Patients/PatientsContainer.tsx
+++ src/presentation/Patients/PatientsContainer.tsx
@@ -1,7 +1,62 @@
-import PatientsView from './PatientsView'
+import { useDispatch, useSelector } from "react-redux";
+import PatientsView from "./PatientsView"
+import { AppDispatch, RootState } from "../../state/store";
+import { getPacientsAsync } from "../../state/patientsSlice";
+import { useEffect, useState } from "react";
+import { CONSTANTS, ROUTES } from "../../constants";
+import { Loading } from "../../components";
+import PatientUseCase from "../../usecase/PatientUseCase";
+import { Patient } from "../../entities/patients";
+import { useNavigate } from 'react-router-dom';
+
 
 const PatientsContainer = () => {
-    return <PatientsView />
+    const [patientsList, setPatientsList] = useState<Patient[]>([])
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+
+    const { login } = useSelector((state: RootState) => state.login);
+    const { patients } = useSelector((state: RootState) => state.patients);
+
+    // On Mount, fetch patients list.
+    useEffect(() => {
+        dispatch(getPacientsAsync(login.token as string));
+    }, []);
+
+    useEffect(() => {
+        setPatientsList(patients)
+    }, [patients]);
+
+
+    // Handle Filter Users.
+    const handlerSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+
+        const useCaseInstance = PatientUseCase();
+        if (useCaseInstance && useCaseInstance.filterPatients) {
+            const response = useCaseInstance.filterPatients(e.target.value, patients);
+            setPatientsList(response)
+        }
+    }
+
+    // Redirect to add user page.
+    const handleAddUser = () => {
+        navigate(ROUTES.PATIENTS_ADD)
+    }
+
+
+    // Loading Flag Pattern.
+    if (!patients.length) return <Loading />;
+
+    // Render PatientsView Component.
+    return <div className=" p-6 w-auto">
+        <PatientsView
+            handlerSearch={handlerSearch}
+            handleAddUser={handleAddUser}
+            patients={patientsList}
+            tableHeader={CONSTANTS.TABLE_HEADER}
+        />
+    </div>
 }
 
 export default PatientsContainer
--- src/presentation/Patients/PatientsView.tsx
+++ src/presentation/Patients/PatientsView.tsx
@@ -1,6 +1,29 @@
-const PatientsView = () => {
+import { Button, Search, Table } from "../../components"
+import { Patient } from "../../entities/patients"
+
+interface PatientsViewProps {
+    patients: Patient[]
+    tableHeader: string[]
+    handleAddUser: () => void
+    handlerSearch: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const PatientsView = ({ patients, tableHeader, handlerSearch, handleAddUser }: PatientsViewProps) => {
     return (
-        <div>PatientsView</div>
+        <>
+            <div className="flex  justify-between m-4">
+                <Search handlerSearch={handlerSearch} />
+                <Button
+                    className="btn-primary"
+                    text="Agregar Paciente"
+                    onClick={handleAddUser}
+                />
+            </div>
+
+            <div className="divider"></div>
+
+            <Table header={tableHeader} data={patients} />
+        </>
     )
 }
 
