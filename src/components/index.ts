98bd7cac4de8ec9183ff0617e49d720d3fa09a4d
pdelbert <pmarcadel@gmail.com>
2025-03-06 14:40:54 -0300

    Adding - Alert Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,12 +3,13 @@ import Loading from './Loading'
 import Table from './Table'
 import Search from './Search'
 import Button from './Button'
+import Alert from './Alert'
 
 export {
     NavBar,
     Loading,
     Table,
     Search,
-    Button
-    
+    Button,
+    Alert
 }
